0c423da1a8d0cb336bd820c2c52d4bff559aa762
RammiyaSR <ramyashunmugam@gmail.com>
2025-04-28 15:07:39 +0530

    adding files and commit to main branch

---
+++ pattern1.py
@@ -0,0 +1,16 @@
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+# row_number = 1
+# for row_number in range(6):
+#     for i in range(row_number):
+#         print(row_number,end="")
+#     print()
+
+row_number = 1
+for row_number in range(1,6):
+    print(str(row_number)*row_number,end="")
+    print()
